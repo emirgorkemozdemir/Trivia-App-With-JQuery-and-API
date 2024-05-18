cebb06d2c17e9c26ca36cc2a084a1e37af5c4d2c
Emir Görkem Özdemir <emirgorkemozdemir@gmail.com>
2024-05-18 21:22:34 +0300

    Add files via upload

--- scripts/load_questions.js
+++ scripts/load_questions.js
@@ -1,3 +1,16 @@
+class question
+{
+constructor(_text,_correctanswer,_wronganswer1,_wronganswer2,_wronganswer3)
+{
+   this.text=_text;
+   this.correctanswer=_correctanswer;
+   this.wronganswer1=_wronganswer1;
+   this.wronganswer2=_wronganswer2;
+   this.wronganswer3=_wronganswer3;
+}
+}
+
+
 async function get_questions()
 {
 	const url = "https://opentdb.com/api.php?amount=30&type=multiple";
@@ -11,8 +24,21 @@ async function get_questions()
 	   const result = await response.text();
 
 	   let value = JSON.parse(result);
+       let questions_from_api = [];
+       for(i=0;i<30;i++)
+        {
+           let xquestion = new question(
+           value['results'][i]['question'],
+           value['results'][i]['correct_answer'],
+           value['results'][i]['incorrect_answers'][0],
+           value['results'][i]['incorrect_answers'][1],
+           value['results'][i]['incorrect_answers'][2]);
 
-	   console.log(value);
+           questions_from_api.push(xquestion);
+        }
+       // 0 - 30 indexleri dolaşacağım.
+       // let xquestion = new question(value[]['question']);
+	   console.log(questions_from_api);
 	}
 	catch(error)
 	{
@@ -20,4 +46,4 @@ async function get_questions()
 	}
 }
 
-get_questions();
+
